9221194b31862079b7ee7bfe5bb5efeb94417fe7
Ricardo Mumm <ricardogmmattos@gmail.com>
2023-04-28 10:39:09 -0300

    Aula 5

--- velha.js
+++ velha.js
@@ -1,6 +1,26 @@
 var jogador = null;
+var jogadorSelecionado = document.getElementById("jogador-selecionado");
 
+mudarJogador('X')
 
 function escolherQuadrado (id){
-    console.log(id)
+    
+    var quadrado = document.getElementById(id);
+
+    quadrado.innerHTML = jogador;
+    quadrado.style.color = "#000";
+
+    if (jogador === 'X') {
+        jogador = 'O';
+    } else {
+        jogador = 'X';
+    }
+
+    mudarJogador(jogador);
+}
+
+function mudarJogador(valor) {
+
+    jogador = valor;
+    jogadorSelecionado.innerHTML = jogador;
 }
